5603c3e406ed35fb11cfa72949abb693c4ba5e63
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-27 08:02:30 -0600

    Created the program along with the runner.

---
+++ src/popups/controller/PopupRunner.java
@@ -0,0 +1,10 @@
+package popups.controller;
+
+public class PopupRunner
+{
+	public static void main (String [] args)
+	{
+		PopupController popupAppController = new PopupController();
+		popupAppController.start();
+	}
+}
