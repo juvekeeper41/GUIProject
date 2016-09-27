e69424f559b723a7b8862f3bfcd167b04f987c73
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-27 09:53:24 -0600

    Made a new Pop-Up window.

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -1,10 +1,27 @@
 package popups.controller;
 
+import popups.view.PopupViewer;
+
 public class PopupController
 {
+	
+	private PopupViewer display;
+	
+	public PopupController()
+	{
+		display = new PopupViewer();
+	}
 
 	public void start()
 	{
+		String answer = "sample";
+		while(answer != null && !answer.equals(""))
+		{
+			
+		display.displayMessage("Hi there :D");
 		
+		answer = display.collectResponse("What is going on?");
+		display.displayMessage(answer + " is what you said.");
+		}
 	}
 }
