3660a8637e03fe33fa7c330d8cf14462c82c187f
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-29 08:45:25 -0600

    Used try/catch methods.

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -22,6 +22,48 @@ public class PopupController
 		
 		answer = display.collectResponse("What is going on?");
 		display.displayMessage(answer + " is what you said.");
+		
 		}
 	}
+	
+	/**
+	 * Checks if the supplied String can be parsed to a double value.
+	 * @param potentialValue The string to test.
+	 * @return Whether the value could be parsed, true if it can be parsed, false otherwise.
+	 */
+	
+	private boolean isDouble(String potentialValue)
+	{
+		boolean isParseable = false;
+		
+		try
+		{
+			double test = Double.parseDouble(potentialValue);
+			isParseable = true;
+			}
+			catch(NumberFormatException notDouble)
+			{
+				display.displayMessage("This is not a double value =:<");
+			}
+		
+			return isParseable;
+		}
+	
+		private boolean isInteger(String potentialNumber);
+		{
+			boolean isParseable = false;
+		
+			try
+			{
+			int test = Integer.parseInt(potentialNumber);
+			isParseable = true;
+			}
+				catch(NumberFormatException notInt)
+				{
+					display.displayMessage(notInt.getMessage());
+					display.displayMessage("Type in an integer next time!");
+				}
+				
+	}
 }
+
