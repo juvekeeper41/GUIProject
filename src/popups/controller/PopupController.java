e68750b6d42cd72d9324dcad734fc83ec4b2f04f
juvekeeper41 <juvekeeper41@gmail.com>
2016-10-04 09:28:41 -0600

    Tried out different methods to count and compare.
    .add(index, value)
    .add(value)
    .size()
    .remove(index)
    .get(index)
    .set(index, value)

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -77,10 +77,62 @@ public class PopupController
 		
 		private void learnLists()
 		{
-			display.displayMessage(thingyList.size() + " is the size of the list.");
-			Thingy testThingy = new Thingy();
-			thingyList.add(testThingy);
-			display.displayMessage(thingyList.size() + " is the size of the list.");
+		display.displayMessage(thingyList.size() + " is the size of the list.");
+		Thingy testThingy = new Thingy();
+		thingyList.add(testThingy);
+		display.displayMessage(thingyList.size() + " is the size of the list.");
+			
+		for (int index = 0; index < 5; index++)
+		{
+		Thingy addThingy = new Thingy();
+		thingyList.add(addThingy);
+		display.displayMessage(addThingy.getWords());
 		}
+			
+		display.displayMessage(thingyList.size() + " is the size");
+			
+		thingyList.get(3).setWords("Look, I changed a value");
+		Thingy temp = thingyList.get(4);
+		temp.setWords("stuff"); 
+			
+		for ( int index = 0; index < thingyList.size(); index ++)
+		{
+			display.displayMessage(thingyList.get(index).getWords());
+		}
+			
+		thingyList.add(3, new Thingy());
+			
+		display.displayMessage("The size is: " + thingyList.size());
+			
+		thingyList.remove(0); //I no longer have access to this object because there is no '=' sign.
+			
+		display.displayMessage("The size is " + thingyList.size());
+			
+		Thingy deletedThingy = thingyList.remove(3);
+		display.displayMessage(deletedThingy.getWords() + ", and the size of the list is " + thingyList.size());
+			
+		thingyList.set(0, deletedThingy);
+			
+		for(int index = 0; index < thingyList.size(); index++)
+		{
+			display.displayMessage(thingyList.get(index). getWords());
+		}
+		
+		for(Thingy currentThingy : thingyList)
+		{
+			display.displayMessage(currentThingy.getWords());
+		}
+
+		for(int index = 0; index < thingyList.size(); index++)
+		{
+			display.displayMessage(thingyList.get(index). getWords());
+		}
+		
+		for(Thingy derpThingy : thingyList)
+		{
+			display.displayMessage(derpThingy.getWords());
+		}
+		
+	}
 }
 
